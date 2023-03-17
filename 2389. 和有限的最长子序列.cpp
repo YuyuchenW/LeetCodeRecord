b0da77bac230fa0a5e1755dca849e31f5246486e
CXS <529548883@qq.com>
2023-03-17 16:52:59 +0800

    init vscode

--- 2389. 和有限的最长子序列.cpp
+++ 2389. 和有限的最长子序列.cpp
@@ -2,12 +2,30 @@
 using namespace std;
 class Solution {
 public:
-    vector<int> searchA(vector<int>& nums,const int& target)
+
+    int searchAns(vector<int>& nums,int target)
     {
         int left = 0;
-        int rigth = 0;
+        int right = 0;
+        int result = 0;
+        int sum = 0;
+        while (right<nums.size())
+        {
+            if (sum<=target)
+            { 
+                ++right;
+
+            }
+            
+        }
+        
+        
     }
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        for (size_t i = 0; i < queries.size(); i++)
+        {
+
+        }
         
     }
 };
